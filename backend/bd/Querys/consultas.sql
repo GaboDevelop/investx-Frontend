e5cb57cdee193d9232c0490ba194165d9dc286ce
Maxikk26 <maximiliano.bogo@gmail.com>
2019-06-22 11:17:10 -0400

    consultas

---
+++ backend/bd/Querys/consultas.sql
@@ -0,0 +1,8 @@
+﻿/*Listado de usuarios con sus roles*/
+
+SELECT U.idUsuario,U.correo,U.contrasena,U.nombre,U.segundoNombre,U.apellido,U.segundoApellido,R.nombreRol,U.active
+FROM USUARIO U
+INNER JOIN ROL_USUARIO RU
+ON U.idUsuario = RU.idUsuario
+INNER JOIN ROL R
+ON R.idRol = RU.idRol
