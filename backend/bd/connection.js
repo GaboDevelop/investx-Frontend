4eeee36885e10d40a89ff0cdecd65b716dec70c0
OscarM <oscarmarquez2218@gmail.com>
2019-07-05 20:06:43 -0400

    Cambios en la conexion

--- backend/bd/connection.js
+++ backend/bd/connection.js
@@ -3,16 +3,22 @@ const pg = require("pg");
 
 const connectionString = "postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"; */
 const pass = "";
-const connectionString = "postgres://root:"+pass+"@localhost:3360/practica";
+const connectionString = "postgres://postgres:"+pass+"@localhost:3360/prueba";
 
 const client = new pg.Client(connectionString);
 
 client.connect()
   .then(()=>{
-    console.log('Conectado a PostgreSQL');
+    console.log('Conectado al servidor');
+    const query = "SELECT * FROM usuario"
+    return client.query(query);
+  }).then((result)=>{
+    console.log('result?',result);
   })
   .catch((err) => {
     console.log('err',err);
-});
+  });
+
+
 
 module.exports = client;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,9 @@
 const API_PATH = "/api";
-const pgConnection = require("../bd/connection");
+const client = require("../bd/connection");
 
 module.exports = app => {
   app.get(`${API_PATH}/usuarios`, (req, res) => {
-    pgConnection.query("SELECT * FROM usuario", (err, rows, fields) => {
+    client.query("SELECT * FROM usuario", (err, rows, fields) => {
       if (!err) {
         res.json(rows);
       } else {
