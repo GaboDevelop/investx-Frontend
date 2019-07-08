c00083d27f4e268e94b69d0f5d9efb9d86b6bbc8
OscarM <oscarmarquez2218@gmail.com>
2019-07-08 00:07:13 -0400

    Agregado consultaServices

---
+++ backend/service/consultaService.js
@@ -0,0 +1,154 @@
+const client = require('../bd/connection.js');
+
+
+/* ----------- LISTADO DE USUARIOS CON SUS ROLES -------------*/
+
+/*------------ HOLOGRAMA DE LA INVESTIGACION -----------------*/
+
+/* -------------TABLA DE OPERACIONALIZACION -------------------*/
+
+/* --------------- TABLA DE ESPECIFIACIONES ------------------*/
+
+/*---------------TABLA DE POBLACION Y MUESTRA ----------------*/
+
+/*------------ TABLA DE LOS ARGUMENTOS DE LA JUSTIFICACION -------*/
+
+/* ----------- LISTADO DE FICHAS POR LOS DIFERENTES TIPOS DE CATEGORIAS, TIPOLOGIAS E INFORMACION CONSTITUTIVA -------------------*/
+
+/*------------- LISTADO DE FICHAS POR CONDICION DE USO EN EL PROYECTO --------------*/
+
+/*-------------- LISTADO DE TIPOS DE INVESTIGACION Y SUS NIVELES -------------------*/
+
+/* ------------- LISTADO DE ARGUMENTOS DE LA JUSTIFICACION FILTRADO POR LOS DIFERENTES ELEMENTOS CONSTITUTIVOS ---------------*/
+
+/*------------ POR TEMPORALIDAD */
+
+/*------------ POR CONTEXTO */
+
+/* ----------- POR UNIDAD DE ESTUDIO */
+
+/* ----------- POR EVENTO */
+
+
+/* ----------- POR TIPO DE INVESTIGACION*/
+
+
+/* ------------------ INSTRUMENTO POR EVENTO --------------------*/
+
+
+/* -------------------- ESQUELETO DEL INFORME DEL PROYECTO -----------------*/
+
+
+
+/* ------------- REPORTE DE CALIDAD DEL PROYECTO DE ACUERDO A LAS MATRICES PROPUESTAS POR HERNANDEZ Y BAPTISTA -------------------*/
+
+
+
+/* -------------- HISTORIAL DE MODIFICACIOES DEL PROYECTO -------------*/
+
+
+
+/* -------------- LISTA INSTITUCIONES DE INVESTIGACION ---------------*/
+
+
+
+/* ------------------ LISTADO DE CONTEXTOS ATENDIDOS --------------*/
+
+
+
+/* ------------------ LISTADO DE PROYECTOS ABORDADOS FILTRADO POR LOS DIFERENTES ELEMENTOS CONSTITUTIVOS --------------*/
+
+
+
+/* -------------- POR CONTEXTO*/
+
+
+/* -------------- POR TEMPORALIDAD*/
+
+
+/* -------------- POR UNIDAD DE ESTUDIO*/
+
+
+/* -------------- POR POBLACION*/
+
+
+/* -------------- POR MUESTRA*/
+
+
+/* -------------- POR EVENTO*/
+
+
+/* -------------- POR TIPO DE INVESTIGACION*/
+
+
+/* -------------- POR NIVEL DE INVESTIGACION*/
+
+
+/* ------------------- LISTADO DE TIPOS DE EVENTOS (clase de enventos) ---------------------*/
+
+
+/* --------------------- LISTADO DE TIPOS DE UNIDADES INFORMACION ---------------*/
+
+/*----------------- LISTADO DE NIVELES DE LOS NIVELES DE LA FUNDAMENTACION POSIBLES -------------------*/
+/* --------- TODOS LOS NIVELES*/
+
+
+/* --------- DESCRIPTIVO*/
+
+
+/* --------- DESCRIPTIVO Y ANALITICO*/
+
+
+/* --------- DESCRIPTIVO Y COMPARATIVO*/
+
+
+/* --------- DESCRIPTIVO Y EXPLICATIVO*/
+
+
+/* --------- DESCRIPTIVO Y PREDICTIVO*/
+
+
+/* ----------------------- LISTADO DE MODALIDADES POR TIPO ---------------------*/
+/* --------- PROYECTIVA*/
+
+
+/* ------------------------ LISTADO DE TIPOS DE TECNICAS POSIBLES DE RECOLECCION DE INFORMACION ------------------------*/
+
+
+/* ------------------------- LISTADO DE TIPOS DE TECNICAS POSIBLES DE ANALISIS ---------------------*/
+
+
+/*-------------------------- LISTADO DE INVESTIGACIONES POR DISCIPLINA O CONDICION TEMATICA -----------------------------*/
+
+
+/* ------------------------- FORMUALRIOS PARA LA CARGA DE CADA UNO DE LOS ELEMENTOS CONSTITUTIVOS DE UN PROYECTO ---------------------------*/
+
+
+/* ------------------------ TABLA HOLOPRAXICA -----------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
